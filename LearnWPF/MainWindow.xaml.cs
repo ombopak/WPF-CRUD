a32619830065ad09a620d32a2f31317d1b09ecc5
omsyai <syaifudinkomputer@gmail.com>
2022-12-31 14:51:21 +0700

    USING API

--- LearnWPF/MainWindow.xaml.cs
+++ LearnWPF/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,30 +17,77 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using LearnWPF.Model;
 using LearnWPF.Presenter;
+using Newtonsoft.Json;
 
 namespace LearnWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
     public partial class MainWindow : Window
     {
+        // Set the base address of the API
+        string baseAddress = "https://localhost:5001/api/Books/";
+        string editBook = "EditBook?BookId={id}&BookTitle={title}&ISBN={isbn}&PublisherName={publisherName}&AuthorName={authorName}&CategoryName={categoryName}";
+        string addBook = "AddBook?Title={title}&Isbn={isbn}&PublisherName={publisherName}&AuthorName={authorName}&CategoryName={categoryName}";
+
+        
+
+
         public MainWindow()
         {
             InitializeComponent();
             FillGridView();
+
+
+            // Create a new HttpClient and set its base address
+           
+
+
+            // Send a GET request to the API and get the response asynchronously
+            //Task<HttpResponseMessage> responseTask = client.GetAsync("endpoint");
+
+            // Wait for the response to complete
+           // responseTask.Wait();
+
+            // Get the response
+            //HttpResponseMessage response = responseTask.Result;
+
+            // Get the content of the response
+            //HttpContent content = response.Content;
+
+            // Read the content as a string
+           // string result = content.ReadAsStringAsync().Result;
+
+            // Do something with the result
+           // Console.WriteLine(result);
         }
 
         void FillGridView()
         {
-            List<Book> bookList = new List<Book>();
+            //USING REST API
+            string getAllBooks = "GetAllBooks";
 
-            Book book = new Book();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(baseAddress);
 
-            bookList = book.GetBooks();
+            Task<HttpResponseMessage> responseTask = client.GetAsync(getAllBooks);
+            responseTask.Wait();
+            HttpResponseMessage response = responseTask.Result;
+            HttpContent content = response.Content;
+            string result = content.ReadAsStringAsync().Result;
+            List<Book> books = JsonConvert.DeserializeObject<List<Book>>(result);
+            dataGridViewBooks.ItemsSource = books;
 
-            dataGridViewBooks.ItemsSource = bookList;
-
+            //USING SQL SERVER CONNECTION
+            //List<Book> bookList = new List<Book>();
+            //Book book = new Book();
+            //bookList = book.GetBooks();
+            //dataGridViewBooks.ItemsSource = bookList;
+            
         }
 
         private void btnNewBookForm_Click(object sender, RoutedEventArgs e)
@@ -59,20 +108,17 @@ namespace LearnWPF
 
         void EditBook()
         {
+            //USING DATABASE SQL CONNECTION
             try
-            {
-                
-                Book row = dataGridViewBooks.SelectedCells[0].Item as Book;
-
+            {                
+               Book row = dataGridViewBooks.SelectedCells[0].Item as Book;
                 int bookId = row.BookId;
-
                 // Debug.WriteLine(bookId);
-
-                EditBookWindow editBookWindow = new EditBookWindow(bookId);
-                editBookWindow.ShowDialog();
-                
+                 EditBookWindow editBookWindow = new EditBookWindow(bookId);
+               editBookWindow.ShowDialog();
+               
             }
-            catch
+             catch
             {
                 string message = "Please choose book to edit!";
                 var dr = MessageBox.Show(message, "Warning", MessageBoxButton.OK);
@@ -105,9 +151,27 @@ namespace LearnWPF
 
                     try
                     {
+                        Debug.WriteLine(bookId);
+                        string deleteBook = $"DeleteBook?BookId={bookId}";
+
+                        HttpClient client = new HttpClient();
+                        client.BaseAddress = new Uri(baseAddress);
 
 
-                        book.DeleteBook(bookId);
+                        HttpResponseMessage responseTask = client.DeleteAsync(deleteBook).Result;
+
+                        if(responseTask.StatusCode == HttpStatusCode.OK)
+                        {
+                            MessageBox.Show("Book Deleted");
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Something Wrong, Book didnt get deleted");
+                        }
+
+
+                        //book.DeleteBook(bookId);
                         FillGridView();
                     }
                     catch
